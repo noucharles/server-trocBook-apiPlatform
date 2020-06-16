3b72d83897aae1ce53b9baaf194286a71c207408
noucharles <noucharles@gmail.com>
2020-06-16 21:33:24 +0100

    Ajout des assertions et resolutions du pb d'hier

--- src/Entity/Annonce.php
+++ src/Entity/Annonce.php
@@ -33,20 +33,21 @@ class Annonce
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"annonce_read", "user_read"})
-     * @Assert\NotBlank(message="Le ville est obligatoire")
+     * @Assert\Length(min="4", max="30", minMessage="Le nom du quartier doit faire au moins 4 caractéres", maxMessage="Le nom du quartier doit faire maximun 30 caractéres")
+     * @Assert\NotBlank(message="Le quartier est obligatoire")
      */
     private $ville;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"annonce_read", "user_read"})
-     * @Assert\NotBlank(message="La classe ou niveau est obligatoire")
      */
     private $classe;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"annonce_read", "user_read"})
+     * @Assert\Length(min="3", max="25", minMessage="Le titre du livre doit faire au moins 3 caractéres", maxMessage="Le titre du livre doit faire maximun 25 caractéres")
      * @Assert\NotBlank(message="Le titre est obligatoire")
      */
     private $titre;
@@ -54,20 +55,24 @@ class Annonce
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"annonce_read", "user_read"})
+     * @Assert\Length(min="3", max="25", minMessage="Le nom de la maison d'edition doit faire au moins 3 caractéres", maxMessage="Le nom de la maison d'edition doit faire maximun 25 caractéres")
+     * @Assert\NotBlank(message="La maison d'édition est obligatoire")
      */
     private $editeur;
 
     /**
      * @ORM\Column(type="integer")
      * @Groups({"annonce_read", "user_read"})
-     * @Assert\NotBlank(message="Le date de parution est obligatoire")
      * @Assert\Type(type="numeric", message="La date de parution doit etre des chiffres !")
+     * @Assert\NotBlank(message="L'année de parution est obligatoire")
      */
     private $parution;
 
     /**
      * @ORM\Column(type="text")
      * @Groups({"annonce_read", "user_read"})
+     * @Assert\Length(min="10", max="2000", minMessage="La description du livre doit faire au moins 10 caractéres", maxMessage="La description du livre doit faire maximun 2000 caractéres (environ 200 mots)")
+     * @Assert\NotBlank(message="La description du livre est obligatoire")
      */
     private $description;
 
